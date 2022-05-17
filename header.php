f257575b56e71cd6b774ef3d80163f37cd735ff1
Mr Robret <bret.farley@gmail.com>
2022-05-17 11:33:54 -0500

    "more php and css changes to look, functionality"

--- header.php
+++ header.php
@@ -8,14 +8,13 @@
 <body>
     <header class="site-header">
         <div class="container">
-            <h1 class="title-logo">bretfarley.com</h1>
+            <h1 class="title-logo"><a href="<?php echo site_url() ?>">bretfarley.com</a></h1>
             <div class="site-header__menu">
                 <nav class="main-nav">
                     <ul>
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">Resum&#233;</a></li>
-                        <li><a href="#">Work</a></li>
-                        <li><a href="#">About</a></li>
+                        <li><a href="<?php echo site_url('/resume') ?>">Resum&#233;</a></li>
+                        <li><a href="<?php echo site_url('/work') ?>">Work</a></li>
+                        <li><a href="<?php echo site_url('/about') ?>">About</a></li>
                     </ul>
                 </nav>
             </div>
