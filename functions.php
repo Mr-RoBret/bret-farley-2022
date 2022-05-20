09878f3baad4ee1b8175f4187cece83ef936f91f
Mr Robret <bret.farley@gmail.com>
2022-05-20 06:02:28 -0500

    added post formatting

--- functions.php
+++ functions.php
@@ -9,6 +9,9 @@ function portfolio_files() {
 function portfolio_features() {
     register_nav_menu('headerMenuLocation', "Header Menu Location");
     add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+    add_image_size('post-thumbnail-size', 500, 300, true);
+    set_post_thumbnail_size('post-thumbnail-size');
 }
 
 add_action('wp_enqueue_scripts', 'portfolio_files');
--- index.php
+++ index.php
@@ -1,26 +1,35 @@
 <?php
 
 get_header();
- 
+
 ?>
 
-    <div class="interior-page-banner container">
-        <div class="interior-page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/interior-background_cropped_copy.png') ?>);"></div>
-        <div class="interior-page-banner__text">
-            <h1 class="headline headline--large">Work</h1>
-            <h2 class="headline headline--medium">Keep up with my latest projects below:</h2>
-        </div>
+<div class="interior-page-banner container">
+    <div class="interior-page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/interior-background_cropped_copy.png') ?>);"></div>
+    <div class="interior-page-banner__text">
+        <h1 class="headline headline--large">Work</h1>
+        <h2 class="headline headline--medium__index-page">Keep up with my latest projects below:</h2>
     </div>
+</div>
 
-    <div class="main-content container">
-        <div class="main-content__text">
-            <p><?php
-                the_content();
-                ?></p>
-        </div>
+<div class="main-content">
+    <div class="post-items__cards-container">
+        <?php
+        while (have_posts()) {
+            the_post(); ?>
+            <div class="post-item__card">
+                <div class="post-item__card--image"><?php the_post_thumbnail('post-thumbnail-size'); ?></div>
+                <h2 class="headline-medium headline--medium__index-page">
+                    <a href="<?php the_permalink() ?>"><?php the_title() ?></a>
+                </h2>
+                <div class="generic-content"><?php the_excerpt() ?></div>
+            </div>
+        <?php }
+        ?>
     </div>
+</div>
 
-    <?php 
+<?php
 
 get_footer();
 
